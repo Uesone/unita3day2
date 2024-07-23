e622cee41a91da69008ab999c38a406caae40f64
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-07-23 16:52:44 +0200

    Initial commit

---
+++ src/Esercizio1/Esercizio1.java
@@ -0,0 +1,17 @@
+package Esercizio1;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+
+        String string1 = "ciao";
+        String string2 = "hello";
+
+        System.out.println("La stringa \"" + string1 + "\" ha un numero di caratteri pari? " + stringaPariDispari(string1));
+        System.out.println("La stringa \"" + string2 + "\" ha un numero di caratteri pari? " + stringaPariDispari(string2));
+    }
+
+    public static boolean stringaPariDispari(String s) {
+
+        return s.length() % 2 == 0;
+    }
+}
